c4f84dbd7b0cfe57f07281c8a02064069efd50d6
yogikannan31 <yyogi9064@gmail.com>
2026-07-09 12:08:56 +0530

    Updated cart and tracking pages

--- script.js
+++ script.js
@@ -957,3 +957,39 @@ function sendMessage(){
 
     input.value="";
 }
+function addToCart(name, price){
+
+    let cart =
+        JSON.parse(localStorage.getItem('cart')) || [];
+
+    cart.push({
+        name: name,
+        price: price
+    });
+
+    localStorage.setItem(
+        'cart',
+        JSON.stringify(cart)
+    );
+
+    alert(name + " added to cart!");
+}
+function addToCart(name,price){
+
+    let cart =
+    JSON.parse(
+    localStorage.getItem("cart")
+    ) || [];
+
+    cart.push({
+        name:name,
+        price:price
+    });
+
+    localStorage.setItem(
+    "cart",
+    JSON.stringify(cart)
+    );
+
+    alert(name + " added to cart!");
+}
